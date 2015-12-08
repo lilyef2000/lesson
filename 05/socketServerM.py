69861c676159954be55ee3976d006821e50e2c4a
lilyef2000 <lilyef2000@163.com>
2015-12-08 22:34:54 +0800

    add socket thread

--- 05/socketServerM.py
+++ 05/socketServerM.py
@@ -1,15 +1,26 @@
 #!usr/bin/env python
 
 import SocketServer
+import os
+import time
 
 class myTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         print "got connection fron:",self.client_address
         while 1:
             self.data = self.request.recv(8196)
-            print self.data
-            format_data = "\033[32;1m%s \033[0m" %self.data
-            self.request.sendall(format_data.upper())
+            if not data:
+                print "connection lost:",self.client_address
+                time.sleep(1)
+                break
+            else:
+                print "will run this on server:",self.data,"on",time.strftime('%H:%M:%S')
+                cmd = os.popen(self.data)
+                if result == '':
+                    result = 'cmd error!!!'
+                else:
+                    result = "\033[32;1m%s \033[0m" %self.data
+                self.request.sendall(result.upper())
 
 h,p = '',9999
 server = SocketServer.ThreadingTCPServer((h,p),myTCPHandler)
